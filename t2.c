dbbe1dbd3c4d267e37ae68ae836acd94fed43b1e
Dylan Neve <dylanneve1@gmail.com>
2024-10-18 15:04:43 +0100

    Update t2.c

--- t2.c
+++ t2.c
@@ -4,7 +4,14 @@
 int number_comparisons = 0;
 int number_swaps = 0;
 
+// Function to swap two integer values
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
 
+// Selection Sort Algorithm
 void selectionSort(int array[], int size) { 
     for (int i = 0; i < size; i++) {
       int minum = i;
@@ -14,15 +21,26 @@ void selectionSort(int array[], int size) {
         }
       }
       if (minum != i) {
-        int cached = array[minum];
-        array[minum] = array[i];
-        array[i] = cached;
+        swap(&array[minum], &array[i]);
       }
     }
-} 
+} /* Time Complexity: O(n^2) */
 
 void insertionSort(int array[], int size) {
-    //TODO
+    for (int i = 0; i < size; i++) {
+      int key = array[i];
+      int j = i - 1;
+
+      if (array[j] <= key) {
+        continue;
+      }
+
+      while (j >= 0 && array[j] > key) {
+        array[j + 1] = array[j];
+        j--;
+      }
+
+      array[j+1] = key;
 }
 
 void quickSort(int array[], int size) {
